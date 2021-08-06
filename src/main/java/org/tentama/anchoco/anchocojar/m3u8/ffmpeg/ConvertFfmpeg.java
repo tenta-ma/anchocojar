f495b62f0b6db40ad210b1fee2f60b56083dbe61
新田八大 <nittayto@pm.nttdata-ccs.co.jp>
2021-08-06 14:40:36 +0900

    サムネイルで利用できそうな色々なオプション追加

--- src/main/java/org/tentama/anchoco/anchocojar/m3u8/ffmpeg/ConvertFfmpeg.java
+++ src/main/java/org/tentama/anchoco/anchocojar/m3u8/ffmpeg/ConvertFfmpeg.java
@@ -69,14 +69,43 @@ public class ConvertFfmpeg {
         File outputFolder = Paths.get(movieRootPath, targetPath).toFile();
 
         try {
+            String filePath = outputFolder.getAbsolutePath() + File.separator + "thumbnail.jpg";
+
             String ffmpeg = Loader.load(org.bytedeco.ffmpeg.ffmpeg.class);
-            // 先頭から100フレーム内で、もっともサムネイルにふさわしい絵をthumbnail.jpgに保存のような感じっぽい
+            // option :
+            // http://mobilehackerz.jp/archive/wiki/index.php?%BA%C7%BF%B7ffmpeg%A4%CE%A5%AA%A5%D7%A5%B7%A5%E7%A5%F3%A4%DE%A4%C8%A4%E1
+
+            // 時間とか指定しないと、先頭から100フレーム内で、もっともサムネイルにふさわしい絵をthumbnail.jpgに保存のような感じっぽい
             ProcessBuilder pb = new ProcessBuilder(ffmpeg,
+                    // 「loglevel」 : ログレベル、まあみてもよくわかんないし、quiet(ログださない)
+                    // 作成できなくてもプログラム上ではエラーにならないのでerrorにした
+                    "-loglevel", "error",
+                    // 「-y」 : overwriteでもnot asking。バックグランド処理にするので、この設定は必須
+                    "-y",
+                    // 「-ss」 : 秒数、この場合3秒～かららしい
+                    // "-ss", "3",
+                    // 「-i」 : input file (resource)
                     "-i", movieFile.getAbsolutePath(),
-                    "-vf", "thumbnail",
+                    // "scale", "640:480",
+                    // 「-s」 : サイズ "NNNxNNN"で横x縦の指定。例：よくあるPCモニター"1920x1080"
+                    // "-s", "375x667",
+                    // 「-aspect」 : アスペクト比 "16:9"のような形で指定、つけてみたけどよくわからなかった
+                    // "-aspect", "3:8",
+                    // 「-vf」 : 「-filter：v」のalias、よくわからなかった
+                    // vf指定「scale」 : サイズを指定できる 「-s」に似ている 「scale=375:667」
+                    // 640:-1とやると、横幅を640にして、元のアスペクト比を保持してできる、だってさ
+                    // "-vf", "thumbnail",
+                    // "-vf", "scale=375:667",
+                    // 「-frames:v, -vframes」 : 取得するframe数、サムネならframeで十分なので1
                     "-frames:v", "1",
-                    outputFolder.getAbsolutePath() + File.separator + "thumbnail.jpg");
+                    // 「-qscale」 : 「-b」だとビットレート指定。大雑把に言うと、画質のqu固定量子化係数
+                    // "-qscale", "32",
+                    // 「-q:v」 : jpegの圧縮率 1～32のようである
+                    "-q:v", "32",
+                    filePath);
             pb.inheritIO().start().waitFor();
+
+            log.info("create thumbnail : {}", filePath);
 
         } catch (InterruptedException e) {
             log.error("InterruptedException error.", e);
